44dce7d2530173b23072d9d9210b513be06f4fdd
josephjacksonw <josephjacksonw@gmail.com>
2022-08-19 17:54:50 -0400

    added optional js functions to the page

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,13 @@
 //Business Logic
-function buildArr(e) {
+function buildArr(e, e2) {
   let outArr = []
   for (i=0; i <= e; i++) {
     if (i.toString().includes("3")) {
+      if (e2 != "") {
+        outArr.push("Won't you be my neighbor " + e2 + "?")
+      } else {
       outArr.push("Won't you be my neighbor?");
+      }
     } else if(i.toString().includes("2")) {
       outArr.push("Boop!")
     } else if(i.toString().includes("1")) {
@@ -15,20 +19,42 @@ function buildArr(e) {
   return outArr;
 }
 
-function welcomeMessage(e) {
-  let output = buildArr(e)
+function welcomeMessage(e, e2) {
+  let output = buildArr(e, e2)
   return output.join(", ");
+}
+function reverse() {
+  revResult = []
+  arrResult = document.getElementById("result").innerText.split(", ");
+  for (i=arrResult.length-1; i>=0; i--) {
+    revResult.push(arrResult[i]);
+  }
+  document.getElementById("result").innerText = revResult.join(", ");
 }
 function makeEvil() {
   document.body.style.backgroundColor = "red";
   document.getElementById("reveal").innerText = "RUN!"
   document.getElementById("goodBot").setAttribute("class", "hidden")
   document.getElementById("badBot").removeAttribute("class");
+  document.getElementById("btn3").removeAttribute("class");
+  document.getElementById("btn1").setAttribute("class", "hidden");
+
 }
+function makeGood() {
+  document.body.style.backgroundColor = "lightblue";
+  document.getElementById("reveal").innerText = "You Saved the Neighborhood!"
+  document.getElementById("badBot").setAttribute("class", "hidden")
+  document.getElementById("goodBot").removeAttribute("class");
+  document.getElementById("btn1").removeAttribute("class");
+  document.getElementById("btn3").setAttribute("class", "hidden");
+}
+
 function hideResults() {
   document.getElementById("reveal").setAttribute("class", "hidden")
-  document.getElementById("btn").setAttribute("class", "hidden");
+  document.getElementById("btn1").setAttribute("class", "hidden");
+  document.getElementById("btn2").setAttribute("class", "hidden");
 }
+
 
 //UI Logic
 function formHandler(event) {
@@ -36,12 +62,14 @@ function formHandler(event) {
   hideResults()
   let result;
   const input = document.getElementById("number1").value;
+  const name = document.getElementById("fname").value;
   if (input < 0) {
     result = "Error, input must be a positive integer or 0";
   } else {
-    result = welcomeMessage(input);
+    result = welcomeMessage(input, name);
     document.getElementById("reveal").removeAttribute("class");
-    document.getElementById("btn").removeAttribute("class");
+    document.getElementById("btn1").removeAttribute("class");
+    document.getElementById("btn2").removeAttribute("class");
   }
   
   document.getElementById("result").innerText = result;
