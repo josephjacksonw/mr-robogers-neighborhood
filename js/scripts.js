d918031863ac337d6a054ce4d2b4f4d6e7e5941e
josephjacksonw <josephjacksonw@gmail.com>
2022-08-28 23:06:13 -0400

    Built a way to count how many of each statments the roboger makes

--- js/scripts.js
+++ js/scripts.js
@@ -5,15 +5,19 @@ function buildArr(e, e2) {
     if (i.toString().includes("3")) {
       if (e2 != "") {
         outArr.push("Won't you be my neighbor " + e2 + "?")
+        threeCount += 1
       } else {
-      outArr.push("Won't you be my neighbor?");
+        outArr.push("Won't you be my neighbor?");
+        threeCount += 1
       }
     } else if(i.toString().includes("2")) {
       outArr.push("Boop!")
+      twoCount += 1
     } else if(i.toString().includes("1")) {
       outArr.push("Beep!")
+      oneCount += 1
     } else {
-    outArr.push(i)
+      outArr.push(i)
   }   
   }
   return outArr;
@@ -53,9 +57,15 @@ function hideResults() {
   document.getElementById("reveal").setAttribute("class", "hidden")
   document.getElementById("btn1").setAttribute("class", "hidden");
   document.getElementById("btn2").setAttribute("class", "hidden");
+  document.getElementById("inputData").setAttribute("class", "hidden");
+  threeCount = 0
+  twoCount = 0
+  oneCount = 0
 }
 
-
+let threeCount = 0
+let twoCount = 0
+let oneCount = 0
 //UI Logic
 function formHandler(event) {
   event.preventDefault();
@@ -70,9 +80,19 @@ function formHandler(event) {
     document.getElementById("reveal").removeAttribute("class");
     document.getElementById("btn1").removeAttribute("class");
     document.getElementById("btn2").removeAttribute("class");
+    let count3 = document.getElementById("threeCount")
+    count3.innerText = threeCount
+    let count2 = document.getElementById("twoCount")
+    count2.innerText = twoCount
+    let count1 = document.getElementById("oneCount")
+    count1.innerText = oneCount
+    document.getElementById("inputData").removeAttribute("class")
   }
   
   document.getElementById("result").innerText = result;
+
+
+
   
 }
 
